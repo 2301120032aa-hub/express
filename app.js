f7f1bc3d171419560ca3c5c2e0ead900e3aaa704
okuaki <2301120032aa@cyber-u.ac.jp>
2026-01-28 19:39:11 +0900

    add env

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
 var dogRouter = require('./routes/dog');
 var AdviceSlipRouter = require('./routes/AdviceSlip');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
 app.use('/dog', dogRouter);
 app.use('/AdviceSlip', AdviceSlipRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
